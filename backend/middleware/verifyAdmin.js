f4277fde0052101efe6b93a45c79b118db7727f8
Avesh Raj Singh <aveshrajsingh3@gmail.com>
2025-05-29 22:49:08 +0530

    fix bug in verifyAdmimn

--- backend/middleware/verifyAdmin.js
+++ backend/middleware/verifyAdmin.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
+import { Admin } from "../models/admin.model.js";
 
 const verifyAdmin = async (req, res, next) => {
-    const user = req.user;
+   const user = req.user;
     if (!user) {
         return res.status(401).json({ message: "Unauthorized" });
     }
-
-    const adminId = new mongoose.Types.ObjectId("6823743b572525500b4acdb1");
-
-
-  if (!user._id.equals(adminId)) {
-    return res.status(403).json({ message: "Forbidden" });
+    const isAdmin = user._id.equals(mongoose.Types.ObjectId("683821862fe56bba07fba3a5"))
+    if (!isAdmin) {
+        return res.status(403).json({ message: "Forbidden from admin" });
     }
 
     req.admin = true;
